446f0e8490ccf4a503422210769440b7ab084f12
charpi <charpi@0940a447-4ae1-0310-816d-a8e047a65a0c>
2005-05-26 09:58:23 +0000

    May code fest refactoring.

--- src/xpdojo.erl
+++ src/xpdojo.erl
@@ -31,33 +31,33 @@
 
 -export([test_files/1, test_files/2, default_options/0]).
 
-default_options() ->
+default_options () ->
     [{unit_modules_filter, adlib:ends_with("ut")},
      {unit_functions_filter, adlib:ends_with("test")},
      {report_function, fun simple_report/1}].
 
 test_files (Directory) ->
-    test_files(Directory, default_options()).
+    test_files (Directory, default_options ()).
 
-test_files(Directory, Options) ->
-    Dir = adlib:normalise_path(filename:absname(Directory)),
+test_files (Directory, Options) ->
+    Dir = adlib:normalise_path (filename:absname (Directory)),
     All_options = adlib:update_options (Options, default_options ()),
     Unit = unit (All_options),
     Compile = compile (All_options),
     with (Dir,
-	  [fun find_modules/2,
-	   fun find_differences/2,
-	   Compile,
-	   fun post_compile/2,
-	   fun (_Dir2, Modules) -> acceptance(Unit(Modules)) end],
-	  []).
+          [fun find_modules/2,
+           fun find_differences/2,
+           Compile,
+           fun post_compile/2,
+           fun (_Dir2, Modules) -> acceptance (Unit(Modules)) end],
+          []).
 
 with (Directory, [Fun|T], Acc) ->
     case Fun (Directory, Acc) of
-	{stop, Result} ->
-	    Result;
-	New_acc ->
-	    with (Directory, T, New_acc)
+        {stop, Result} ->
+            Result;
+        New_acc ->
+            with (Directory, T, New_acc)
     end;
 with (_Directory, [], Acc) ->
     Acc.
@@ -65,74 +65,88 @@ with (_Directory, [], Acc) ->
 post_compile (_Directory, {{compiled, Compiled}, {failed, Failed}}) ->
     lists:foreach (
       fun(Module) ->
-	      code:purge(Module),
-	      {module, Module} = code:load_file(Module)
+              code:purge (Module),
+              {module, Module} = code:load_file (Module)
       end,
       Compiled),
     lists:foreach (
       fun(Module) ->
-	      code:purge(Module),
-	      code:delete(Module)
+              code:purge (Module),
+              code:delete (Module)
       end,
       Failed),
-    {length(Compiled) + length(Failed), Compiled}.
+    {length (Compiled) + length (Failed), Compiled}.
 
 find_modules (Directory, _) ->
     case source:erlang_files (Directory) of
-	[] ->
-	    {stop, no_source_files};
-	Files ->
-	    Files
+        [] ->
+            {stop, no_source_files};
+        Files ->
+            Files
     end.
 
 find_differences (Directory, Files) ->
     Loaded_modules = compiling:loaded_modules (Directory),
     case compiling:differences (Loaded_modules, Files) of
-	[] ->
-	    {stop, unchanged};
-	_Changes ->
-	    Files
+        [] ->
+            {stop, unchanged};
+        _Changes ->
+            Files
     end.
 
 compile (Options) when is_list (Options) ->
     {value, {_,  Report_function}} = lists:keysearch (report_function, 1, Options),
     compile (Report_function);
-compile (Report_function) when is_function(Report_function) ->
+compile (Report_function) when is_function (Report_function) ->
     fun (Dir, Files) -> compiling:compile (Dir, Files, Report_function) end.
     
-unit (Options) ->
+unit (Options) when is_list (Options) ->
     {value, {_,  Module_filter}} = lists:keysearch (unit_modules_filter, 1, Options),
     {value, {_,  Function_filter}} = lists:keysearch (unit_functions_filter, 1, Options),
     {value, {_,  Report_function}} = lists:keysearch (report_function, 1, Options),
-    unit (Module_filter, Function_filter, Report_function).
-
-unit (Mod_filter, Fun_filter, Report_function) ->
-    fun({Total_module_count, Compiled_modules}) when length(Compiled_modules) < Total_module_count; Total_module_count == 0 ->
-	    [{modules, Total_module_count, Compiled_modules}];
-       ({Total_module_count, Compiled_modules}) ->
-	    test_pass(Mod_filter, Fun_filter, Report_function, Compiled_modules, unit, [{modules, Total_module_count, Compiled_modules}])
+    unit ({Module_filter, Function_filter, Report_function});
+unit (Options) when is_tuple (Options) ->
+    fun ({Module_count, Compiled_modules}) ->
+            Work = 'work?' (Module_count, Compiled_modules),
+            unit_work (Options, Work, Module_count, Compiled_modules)
     end.
+
+'work?' (0,_) ->
+    no_work;
+'work?' (Module_count, Compiled_modules) when length (Compiled_modules) < Module_count ->
+    no_work;
+'work?' (_,_) ->
+    has_work.
+
+unit_work (Options, has_work, Module_count, Compiled_modules) ->
+    test_pass (Options, Compiled_modules, unit, [{modules, Module_count, Compiled_modules}]);
+unit_work (_, no_work, Module_count, Compiled_modules) ->
+    [{modules, Module_count, Compiled_modules}].
+
 
 acceptance ([{modules, Count, Compiled_modules}]) ->
     [{modules, Count, length (Compiled_modules)}];
 acceptance ([{unit, Total, Successes}, {modules, Count, Compiled_modules}]) when Successes < Total ->
     [{unit, Total, Successes}, {modules, Count, length (Compiled_modules)}];
-acceptance ([{unit, UnitTotal, UnitSuccesses}, {modules, Module_total, Compiled_modules}]) ->
-    test_pass (adlib:ends_with("_acceptance"), adlib:ends_with("_test"), fun(X) -> io:fwrite("~p~n",[X]) end, Compiled_modules, acceptance,
-	       [{unit, UnitTotal, UnitSuccesses}, {modules,Module_total,length(Compiled_modules)}]).
-    
-test_pass (Module_filter, Function_filter, Report_function, Modules, Pass_name, Acc) ->
+acceptance ([Unit_summary, {modules, Module_total, Compiled_modules}]) ->
+    test_pass (
+      {adlib:ends_with ("_acceptance"), adlib:ends_with ("_test"), fun (X) -> io:fwrite ("~p~n",[X]) end}, 
+      Compiled_modules, 
+      acceptance, 
+      [Unit_summary, {modules,Module_total,length(Compiled_modules)}]).
+
+test_pass ({Module_filter, Function_filter, Report_function}, Modules, Pass_name, Acc) ->
     Tests = [X || X <- Modules, Module_filter(X)],
     Results = testing:run_modules (Tests, Function_filter),
     {Total, FailureCount, Failures}
-	= lists:foldl(
-	    fun({Module, Count, []}, {Sum, FailureCount, FailuresAcc}) ->
-		    {Count + Sum, FailureCount, [Module|FailuresAcc]};
-	       ({Module, Count, Failures}, {Sum, FailureCount, FailuresAcc}) ->
-		    {Count + Sum, FailureCount+length(Failures), [{Module,Failures} | FailuresAcc]}
-	    end,
-	    {0, 0, []},
-	    Results),
+        = lists:foldl(
+            fun({Module, Count, []}, {Sum, FailureCount, FailuresAcc}) ->
+                    {Count + Sum, FailureCount, [Module|FailuresAcc]};
+               ({Module, Count, Failures}, {Sum, FailureCount, FailuresAcc}) ->
+                    {Count + Sum, FailureCount+length(Failures), [{Module,Failures} | FailuresAcc]}
+            end,
+            {0, 0, []},
+            Results),
 
     report(Failures, Pass_name, Report_function),
     [{Pass_name, Total, Total - FailureCount} | Acc ].
